53e61ff7861cafb3d037961049e8b88073157b46
PabloSQ93 <pasalqui@hotmail.com>
2023-03-16 10:19:04 +0100

    Nuevo archivo main.py y modificación de la función monitor en okaertool.py

--- python_lib/src/main.py
+++ python_lib/src/main.py
@@ -1,24 +1,36 @@
 import ok.ok as ok
 import okaertool as okt
 from pyNAVIS import *
+import time
+
+time_start = time.perf_counter()
+
 
 # 64 Channels,  Stereo, 16 bits address, recorded using jAER
-NUM_CHANNELS = 64
-BIN_SIZE = 20000
-SETTINGS = MainSettings(num_channels=NUM_CHANNELS, mono_stereo=1, on_off_both=1, address_size=2, ts_tick=1,
-                        bin_size=BIN_SIZE)
+# NUM_CHANNELS = 64
+# BIN_SIZE = 20000
+
+# pyNAVIS settings
+# SETTINGS = MainSettings(num_channels=NUM_CHANNELS, mono_stereo=1, on_off_both=1, address_size=2, ts_tick=1,
+#                         bin_size=BIN_SIZE)
 
 
-device = ok.okCFrontPanel()
-# device.OpenBySerial("")
+device = ok.okCFrontPanel()                 # Número de Serie de la OpalKelly: 1529000BQK
+# device.OpenBySerial("1529000BQK")         # Si NO se descomenta, falla la conexión USB con la OpalKelly
+serial_number = device.GetSerialNumber()
+print('Número de Serie', serial_number)
 device_count = device.GetDeviceCount()
+print('device_count =', device_count)
 for idx in range(device_count):
     print(f"Device[{idx}] Model: {device.GetDeviceListModel(idx)}")
 
-BIT_FILE = '/home/arios/Projects/okaertool/okt_top.bit'
-ok = okt.Okaertool(bit_file=BIT_FILE)
-ok.init()
-ok.select_inputs(inputs=['Port_A'])
-spikes = ok.monitor(buffer_length=(1*1024*1024))
+BIT_FILE = r"C:\Users\PabloSQ_2023\PycharmProjects\Okaertool\okaertool-master\okaertool-master\okt_top.bit"
+opal = okt.Okaertool(bit_file=BIT_FILE)
+opal.init()
+opal.select_inputs(inputs=['Port_A'])
+spikes = opal.monitor(buffer_length=(1*1024*1024), events=300000)
+print('--------------------------')
+time_elapsed = (time.perf_counter() - time_start)
+print('Tiempo Final de Simulación', time_elapsed)
 
-Plots.spikegram(spikes[0], settings=SETTINGS)
+# Plots.spikegram(spikes[0], settings=SETTINGS)
--- python_lib/src/okaertool.py
+++ python_lib/src/okaertool.py
@@ -21,6 +21,10 @@
 
 import ok.ok as ok
 import logging as log
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 
 class Spikes:
@@ -49,7 +53,7 @@ class Okaertool:
     OUTPIPE_ENDPOINT = 0xA0
     INWIRE_COMMAND_ENDPOINT = 0x00
     INWIRE_SELINPUT_ENDPOINT = 0x01
-    NUM_INPUTS = 5
+    NUM_INPUTS = 3
     USB_BLOCK_SIZE = 1024
 
     def __init__(self, bit_file):
@@ -58,6 +62,12 @@ class Okaertool:
         self.id = self.device.GetDeviceID()
         self.bit_file_path = bit_file
         self.inputs = []
+        self.global_timestamp = 0
+
+    def reset_timestamp(self):
+        """
+        Reset self.global_timestamp
+        """
         self.global_timestamp = 0
 
     def init(self):
@@ -91,14 +101,17 @@ class Okaertool:
             if 'Node_out' in inputs:
                 selinput_endpoint_value += 4  # Set 1 in the bit number 2
 
+        print('Valor entrada PIN', selinput_endpoint_value)
+
         self.device.SetWireInValue(self.INWIRE_SELINPUT_ENDPOINT, selinput_endpoint_value)
         self.device.UpdateWireIns()
 
-    def monitor(self, buffer_length):
+    def monitor(self, buffer_length, events):
         """
-        Get the information captured buy the tool and save it in different spikes structs depending on the selected
+        Get the information captured by the tool and save it in different spikes structs depending on the selected
         inputs
         :param buffer_length: Number of bytes to be read from the tool
+        :param events: Number of events collected with non-zero address and timestamp other than /xff/xff/xff/xff
         :return: spikes: List of captured spikes (ts, addr)
         """
         # Enable capture function
@@ -109,21 +122,116 @@ class Okaertool:
         # Read information from the device
         buffer = bytearray(buffer_length)
         num_read_bytes = self.device.ReadFromBlockPipeOut(self.OUTPIPE_ENDPOINT, self.USB_BLOCK_SIZE, buffer)
+        print('Numero de Bytes Leídos', num_read_bytes)
+        buffer_list = list(buffer)
+        # Counter used for counting goods events and debugging
+        q = 1
+        # Empty lists of address and timestamp datas
+        addr_histogram = []
+        ts_histogram = []
         # Loop in the collect data and split the information into the right spike input struct
         for b_idx in range(0, num_read_bytes, 8):  # Each spike is a ts(4 bytes) and addr(4bytes)
-            ts = int.from_bytes(buffer[b_idx:b_idx+4], byteorder='big', signed=False)
-            addr = int.from_bytes(buffer[b_idx+4:b_idx+8], byteorder='big', signed=False)
-            if ts == 0 and addr == 0:  # Null value. Used to fill de USB packet
-                continue
-            # Increase the global timestamp using the differential timestamp received
-            self.global_timestamp += ts
-            # Get the input index that is encoded in the 3 most significant bits.
-            input_idx = (addr & 0xE000_0000) >> 29
-            # Save the global timestamp and the address in the spike list corresponding with the input
-            spikes[input_idx].timestamps.append(self.global_timestamp)
-            spikes[input_idx].addresses.append(addr)
+            # Example of this range(0, num_read_bytes, 8) with spikes = 1 Mb and by USB_BLOCK_SIZE = 1024 bytes -->
+            # Number of iterations: 1048568 / 8 = 131071
+            try:
+                print('-------------Índice b_indx-------------', b_idx)
+                ts = int.from_bytes(buffer[b_idx:b_idx+4], byteorder='big', signed=False)
+                print('Timestamp ts', ts)
+                addr = int.from_bytes(buffer[b_idx+4:b_idx+8], byteorder='big', signed=False)
+                print('addr', addr)
+                # ----------------------------------------------------------------------
+                # if ts == 0 and addr == 0:  # Null value. Used to fill de USB packet
+                #     continue
+                # ----------------------------------------------------------------------
+                if ts == 4294967295 or addr == 0:           # 4294967295 = /xff/xff/xff/xff
+                    continue
+                else:
+                    pass
+                ts_histogram.append(ts)
+                addr_histogram.append(addr)
+                q += 1
+                print('Iteración con timestamp distinto de 4294967295 y dirección distinta de 0. Número =', q)
+                # Increase the global timestamp using the differential timestamp received
+                self.global_timestamp += ts
+                print('global_timestamp', self.global_timestamp)
+                # Get the input index that is encoded in the 3 most significant bits.
+                input_idx = (addr & 0xE000_0000) >> 29
+                # Save the global timestamp and the address in the spike list corresponding with the input
+                spikes[input_idx].timestamps.append(self.global_timestamp)
+                print('Timestamp añadido a', spikes[input_idx])
+                spikes[input_idx].addresses.append(addr)
+                print('Dirección añadida a', spikes[input_idx])
+                # Condition to take the number of events selected
+                if q <= events:
+                    pass
+                else:
+                    break
+            # Except used to neglect the IndexError
+            except IndexError as e:
+                print('ERROR', e)
         # Disable capture function
         self.device.SetWireInValue(self.INWIRE_COMMAND_ENDPOINT, 0x00000000)
         self.device.UpdateWireIns()
+        print('--------------------------')
+        print('Número de eventos correctos =', q)
+        null_events = (num_read_bytes/8)-q
+        print('Número de eventos nulos =', null_events)
+        print('spikes', spikes)
+        print('Número de elementos de TimeStamp', len(ts_histogram))
+        print('Número de elementos de Direcciones', len(addr_histogram))
+        # Counting how many times appears the elements of timestamp and addresses creating a dictionary
+        recounted_ts = Counter(ts_histogram)
+        recounted_addr = Counter(addr_histogram)
+        # Separating the keys and values from dictionaries for timestamp and address and creating lists
+        ts_keys = list(recounted_ts.keys())
+        ts_values = list(recounted_ts.values())
+        addr_keys = list(recounted_addr.keys())
+        addr_values = list(recounted_addr.values())
+        # Transform in string the keys from dictionaries
+        ts_keys_string = [str(i) for i in ts_keys]
+        addr_keys_string = [str(i) for i in addr_keys]
+
+        # # Creating series from the lists
+        # freq_series_ts = pd.Series(ts_values)
+        # freq_series_addr = pd.Series(addr_values)
+
+        # ts_keys_numbers = []
+        # for j in range(0, len(ts_keys)):
+        #     ts_keys_numbers.append(j)
+        #
+        # addr_keys_numbers = []
+        # for j in range(0, len(addr_keys)):
+        #     addr_keys_numbers.append(j)
+
+
+        # Plotting bar figures
+
+        df = pd.DataFrame({'ts_key': ts_keys_string, 'val_ts': ts_values})
+        df.plot.bar(x='ts_key', y='val_ts', rot=0)
+
+        df = pd.DataFrame({'addr_key': addr_keys_string, 'val_addr': addr_values})
+        df.plot.bar(x='addr_key', y='val_addr', rot=0)
+
+        # fig1 = plt.figure(figsize=(10, 10))
+        # fig1.autofmt_xdate()
+        # plt.xlim(min(ts_keys), max(ts_keys))
+        # plt.ylim(min(ts_values), max(ts_values))
+        # ax = freq_series_ts.plot(kind="bar")
+        # ax.set_title("Time Stamps")
+        # ax.set_xlabel("Ts Value")
+        # ax.set_ylabel("Frequency Value")
+        # # ax.set_xticklabels(ts_keys_numbers)
+        #
+        # fig2 = plt.figure(figsize=(10, 10))
+        # fig2.autofmt_xdate()
+        # plt.xlim(min(addr_keys), max(addr_keys))
+        # plt.ylim(min(addr_values), max(addr_values))
+        # ax = freq_series_addr.plot(kind="bar")
+        # ax.set_title("Addresses")
+        # ax.set_xlabel("Addr Value")
+        # ax.set_ylabel("Frequency Value")
+        # # ax.set_xticklabels(addr_keys_numbers)
+
+        plt.show()
 
         return spikes  # spikes
